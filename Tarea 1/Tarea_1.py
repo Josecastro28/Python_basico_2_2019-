462f0e4174a93f21ec184aa165933e75d247fb16
Josecastro28 <49097766+Josecastro28@users.noreply.github.com>
2019-05-03 12:05:11 -0600

    Add files via upload

---
+++ Tarea 1/Tarea_1.py
@@ -0,0 +1,9 @@
+# Valor variable asterisco
+
+N = "************"
+
+# Valor de variable PI
+PI = 3.1415926
+
+print(("%.1s" % N),("%.9s" % PI), ("%.2s" % N), ("%.8s" % PI), ("%.3s" % N), ("%.7s" % PI ), ("%.4s" % N), ("%.6s" % PI ), ("%.5s" % N), ("%.5s" % PI ), ("%.5s" % N) ,("%.4s" % PI ))
+
