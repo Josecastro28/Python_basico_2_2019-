d77f43710092fb93d64800ef9a1bd1c9766b06cb
Josecastro28 <49097766+Josecastro28@users.noreply.github.com>
2019-05-03 12:03:35 -0600

    Tarea 2

---
+++ Ejercicio 1 tarea 2.py
@@ -0,0 +1,12 @@
+#Crear líneas de código en Python que calcule el promedio de los valores contenidos en una lista.
+
+mis_valores = [5, 6, 10, 13, 3, 4]
+
+suma = mis_valores[0] + mis_valores [1] + mis_valores [2] + mis_valores [3] + mis_valores [4] + mis_valores [5]
+
+promedio = suma / 6
+print('El promedio de los numeros en lista es',promedio)
+
+
+
+
